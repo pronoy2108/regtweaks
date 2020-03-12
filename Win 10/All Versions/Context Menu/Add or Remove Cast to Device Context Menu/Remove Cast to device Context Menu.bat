f99a9eec46b85adbe58cd32ede6cceaaa3203f1c
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2020-03-12 15:04:37 +0100

    Enable/Disable Cast to device Context Menu
    You do not need to restart your OS, a explorer.exe refresh is enough to apply the changes, that's the reason why this is a batch script.

---
+++ Win 10/All Versions/Context Menu/Add or Remove Cast to Device Context Menu/Remove Cast to device Context Menu.bat
@@ -0,0 +1,6 @@
+@echo off
+
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked" /V {7AD84985-87B4-4a16-BE58-8B72A5B390F7} /T REG_SZ /D "Play to Menu" /F
+
+taskkill /f /im explorer.exe
+start explorer.exe
